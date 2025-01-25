e97d3fb07e03e73c4ddb17ed0ec4223ee67bc84b
muhammadkashif20 <kashif28@gmail.com>
2025-01-25 13:35:32 +0500

    Register Api Completed Move To Login Api

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import userRouter from "./routers/users.js"
-import authRouter from "./routers/auth.js"
+import userRouter from "./routers/users.js";
+import authRouter from "./routers/auth.js";
 const app = express();
 app.use(express.json());
 const PORT = 5000;
@@ -12,17 +12,14 @@ mongoose
   .then(() => console.log("Mongodb is Connected!"))
   .catch((err) => console.log("Error", err));
 app.use(cors());
-app.use("/users",userRouter)
-app.use("/auth",userRouter)
-
+app.use("/users", userRouter);
+app.use("/auth", authRouter);
 app.get("/login", (req, res) => {
   res.send("Login");
 });
 app.get("/signup", (req, res) => {
   res.send("signup");
 });
-app.get("/logout", (req, res) => {
-    res.send("logout");
-  });
+
 
 app.listen(PORT, () => console.log("Port is Running ON =>", PORT));
--- routers/auth.js
+++ routers/auth.js
@@ -1,19 +1,52 @@
 import express from "express";
 const router = express.Router();
 import Joi from "joi";
+import bcrypt from "bcrypt";
+import "dotenv/config";
+import jwt from 'jsonwebtoken'
+import sendResponse from "../Helpers/sendResponse.js";
+import Users from "../models/Users.js";
 const registerSchema = Joi.object({
-    username: Joi.string().alphanum().min(3).max(30).required(),
-    // password:Joi.(new RegExp('^[a-zA-Z0-9]{3,30}$')),
-    // email:
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  password: Joi.string().min(6),
+  fullname: Joi.string().alphanum().min(3).max(30).required(),
 });
-router.post("/register", (req, res) => {
-    const { error, value } = registerSchema.validate(req.body);
-    if (error) return sendResponse(res, 400, error.details[0].message);
-    res.send("Register");
+const loginSchema = Joi.object({
+    email: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    }),
+    password: Joi.string().min(6),
+  });
+router.post("/register", async (req, res) => {
+  const { error, value } = registerSchema.validate(req.body);
+  console.log("error=> ", error);
+  if (error) return sendResponse(res, 400, null, true, error.message);
+  console.log("value=> ", value);
+  const user = await Users.findOne({ email: value.email });
+  if (user) return sendResponse(res, 400, null, true, "User Already Exist");
+  const hashPassword = await bcrypt.hash(value.password, SaltRounds);
+  value.password = hashPassword;
+  let newUser = new Users({ ...value });
+  newUser = await newUser.save();
+  sendResponse(res, 201, newUser, false, "User Registered Successfully");
 });
-router.post("/login", (req, res) => {});
-router.post("/logout", (req, res) => {
-    console.log("Logout successfully");
+router.post("/login", async (req, res) => {
+  const { error, value } = loginSchema.validate(req.body);
+  console.log("error=> ", error);
+  if (!error) return sendResponse(res, 400, null, true, error.message);
+  console.log("value=> ", value);
+  const user = await Users.findOne({ email: value.email }).lean();
+  if (!user) return sendResponse(res, 400, null, true,"User is Not Registered");
+  const isPasswordValid = await bcrypt.compare(value.password, user.password);
+    if (!isPasswordValid) return sendResponse(res, 400, null, true, "Invalid Credientals");
+    var token = jwt.sign(user, 'shhhhh');
+  let newUser = new Users({ ...value });
+  newUser = await newUser.save();
+  sendResponse(res, 201, newUser, false, "User Registered Successfully");
 });
-export default router;
 
+export default router;
