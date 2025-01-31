b76eee60c52a2ed5610714f38d5dc38b61e8fe85
muhammadkashif20 <kashif28@gmail.com>
2025-01-31 23:04:14 +0500

    changes

--- routers/loanRequest.js
+++ routers/loanRequest.js
@@ -103,31 +103,31 @@ router.post("/verifyPassword", async (req, res) => {
 });
 
 // Get appointments
-router.get("/getAppointment", async (req, res) => {
-  const { city, country } = req.query;
-  let appointmentRequest = null;
-  if (city && country) {
-    appointmentRequest = await Appointment.find({ city, country });
-  } else {
-    appointmentRequest = await Appointment.find();
-  }
-  if (!appointmentRequest) return sendResponse(res, 400, true, null, "Appointment Request Failed");
-  sendResponse(res, 200, false, appointmentRequest, "Appointment Request Successfully");
-});
+// router.get("/getAppointment", async (req, res) => {
+//   const { city, country } = req.query;
+//   let appointmentRequest = null;
+//   if (city && country) {
+//     appointmentRequest = await Appointment.find({ city, country });
+//   } else {
+//     appointmentRequest = await Appointment.find();
+//   }
+//   if (!appointmentRequest) return sendResponse(res, 400, true, null, "Appointment Request Failed");
+//   sendResponse(res, 200, false, appointmentRequest, "Appointment Request Successfully");
+// });
 
 // Add an appointment (example)
-router.post("/addAppointment", async (req, res) => {
-  const { userId, applicationId, appointmentDate, appointmentTime, location } = req.body;
-  const newAppointment = new Appointment({ userId, applicationId, appointmentDate, appointmentTime, location });
-  await newAppointment.save();
-  if (!newAppointment) return sendResponse(res, 400, true, null, "Appointment Request Failed");
+// router.post("/addAppointment", async (req, res) => {
+//   const { userId, applicationId, appointmentDate, appointmentTime, location } = req.body;
+//   const newAppointment = new Appointment({ userId, applicationId, appointmentDate, appointmentTime, location });
+//   await newAppointment.save();
+//   if (!newAppointment) return sendResponse(res, 400, true, null, "Appointment Request Failed");
 
-  // Send appointment confirmation email
-  const user = await Users.findById(userId);
-  if (user) {
-    await sendEmail(user.email, "Appointment Confirmation", `Your appointment has been scheduled on ${appointmentDate} at ${appointmentTime}.`);
-  }
-  sendResponse(res, 201, false, newAppointment, "Appointment Request Successfully");
-});
+//   // Send appointment confirmation email
+//   const user = await Users.findById(userId);
+//   if (user) {
+//     await sendEmail(user.email, "Appointment Confirmation", `Your appointment has been scheduled on ${appointmentDate} at ${appointmentTime}.`);
+//   }
+//   sendResponse(res, 201, false, newAppointment, "Appointment Request Successfully");
+// });
 
 export default router;
