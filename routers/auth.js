7cccb808b550889b411a80af8682c30c9cd37405
muhammadkashif20 <kashif28@gmail.com>
2025-02-21 11:13:17 +0500

    checking err

--- routers/auth.js
+++ routers/auth.js
@@ -13,7 +13,7 @@ const registerSchema = Joi.object({
     .required(),
   email: Joi.string().email().required(),
   fullname: Joi.string().min(3).required(),
-  password: Joi.string().min(6),
+  password: Joi.string().min(6).optional(),
 });
 const loginSchema = Joi.object({
   cnic: Joi.string()
